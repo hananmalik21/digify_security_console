077b66a4e498840510ac15d176a224682b237ab8
Affan Saleem <m.affansalim@gmail.com>
2026-06-27 23:31:04 +0500

    Update sc_bootstrap.dart

--- lib/integration/sc_bootstrap.dart
+++ lib/integration/sc_bootstrap.dart
@@ -1,4 +1,7 @@
+import 'package:digify_core/network/api_client.dart';
+import 'package:digify_core/network/auth_token_storage.dart';
 import 'package:digify_core/providers/current_user_provider.dart';
+import 'package:digify_security_console/integration/sc_network_providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 export 'package:digify_core/providers/current_user_provider.dart'
@@ -15,4 +18,14 @@ List<Override> get securityConsoleProviderOverrides => [
 
 void setActiveEnterpriseId(WidgetRef ref, int? enterpriseId) {
   ref.read(suiteActiveEnterpriseIdProvider.notifier).state = enterpriseId;
+}
+
+/// Binds host auth storage directly to [scApiClientProvider].
+Override buildScApiClientHostOverride(AuthTokenStorage Function(Ref ref) authStorage) {
+  return scApiClientProvider.overrideWith(
+    (ref) => ApiClient(
+      baseUrl: ref.watch(apiBaseUrlProvider),
+      authStorage: authStorage(ref),
+    ),
+  );
 }
